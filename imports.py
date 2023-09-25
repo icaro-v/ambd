209ded8d2420a55fc814fdbc8e42347276d5f3cb
somaicaro <icaro.paula@somasolucoes.com>
2023-09-25 13:15:39 -0300

    style: reposicionar imports

--- imports.py
+++ imports.py
@@ -1,10 +1,8 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QFrame, QVBoxLayout, QDialog, QLabel, QWidget, QFileDialog
 from PySide6.QtCore import Qt, QSize, QCoreApplication
 from funcoes_pg import *
-
 from msg import *
 from tabelas import *
-
 from interface import Ui_migr
 
 import sys, os, socket
