790a943ee600f9803ec06f856a32ca6a1759e7ae
somaicaro <icaro.paula@somasolucoes.com>
2023-09-29 09:50:56 -0300

    docs: atualizar o requirements.txt

--- imports.py
+++ imports.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QFrame, QVBoxLayout, QDialog, QLabel, QWidget, QFileDialog
 from PySide6.QtCore import Qt, QSize, QCoreApplication
+from PySide6.QtGui import QPixmap
 from funcoes_pg import *
 from msg import *
 from tabelas import *
 from interface import Ui_migr
+from PIL import Image
 
-import sys, os, socket
+import sys, os, socket, io
