49358ec01b851c75b25357ed8765a6d0a245083e
somaicaro <icaro.paula@somasolucoes.com>
2024-01-24 16:35:20 -0300

    feat: configurar o icone da aplicacao para aparecer na barra de tarefas e também na janela, no canto superior esquerdo

--- imports.py
+++ imports.py
@@ -1,11 +1,9 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QFrame, QVBoxLayout, QDialog, QLabel, QWidget, QFileDialog
 from PySide6.QtCore import Qt, QSize, QCoreApplication
-from PySide6.QtGui import QPixmap, QCursor, QIcon
+from PySide6.QtGui import QPixmap, QCursor, QIcon, QImage
 from funcoes_pg import *
 from msg import *
 from tabelas import *
 from interface import Ui_migr
 from img_bin import binario
-from time import sleep
-
 import sys, os, socket, io
--- test/testar_img.py
+++ test/testar_img.py
@@ -1,4 +1,4 @@
-import sys
+import sys, base64
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from PyQt5.QtGui import QPixmap
 
@@ -6,51 +6,28 @@ class ImageViewer(QMainWindow):
     def __init__(self, image_path):
         super().__init__()
 
-        image_path = 'imgs/converter.png'
-
-
-        # # Carregar a imagem a partir do caminho especificado
-        # with open(image_path, 'rb') as f:
-        #     image_data = f.read()
-        
-        # Passo 1: Carregar a imagem como dados binários
         with open(image_path, 'rb') as image_file:
             image_binary = image_file.read()
         
         image_base64 = base64.b64encode(image_binary).decode()
-
-        print(image_base64)
-
-        exit()
-
-        # with open('bin.txt', 'w') as bin:
-        #     bin.write(image_base64)
-
-
-        img = base64.b64decode(binario(2))
-
+        img = base64.b64decode(image_base64)
+        
         pixmap = QPixmap()
         pixmap.loadFromData(img)
 
-        # pixmap.loadFromData(image_data)
-
-
-
-        # Criar um QLabel para exibir a imagem
         self.image_label = QLabel(self)
         self.image_label.setPixmap(pixmap)
 
-        # Configurar a janela principal
         self.setWindowTitle("Visualizador de Imagem")
         self.setGeometry(100, 100, pixmap.width(), pixmap.height())
 
-        # Criar um layout para o widget principal
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
 
         layout = QVBoxLayout()
         layout.addWidget(self.image_label)
         central_widget.setLayout(layout)
+
 
 def main(image_path):
     app = QApplication(sys.argv)
@@ -59,4 +36,4 @@ def main(image_path):
     sys.exit(app.exec_())
 
 
-main('imgs/postgresql.png')
+main('imgs/conversor.ico')
